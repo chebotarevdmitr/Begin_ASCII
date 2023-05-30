bce5f5af04dc744f1d87523e57b4285a8ac823ee
DIM <4ebotarov_dim@inbox.ru>
2023-05-30 21:44:15 +0300

    5 numbers message HELLO

--- main.cpp
+++ main.cpp
@@ -14,8 +14,13 @@ printf("Hello ASCII \n");
   char c1,c2,c3,c4,c5;
   cout<<"Enter 5 letters : ";
   cin>>c1>>c2>>c3>>c4>>c5 ;
-  cout<<"ASCII message " << int (c1) <<	" " << int (c2)
-   << " " << int (c3) << " " <<int(c4) << " " << int (c5) ;
+  cout<<"ASCII message : " << int (c1) <<	" " << int (c2)
+   << " " << int (c3) << " " <<int(c4) << " " << int (c5)<<endl ;
+   
+   int q1,q2,q3,q4,q5;
+   cout<<"Enter 5 numbers :  ";
+   cin>>q1>>q2>>q3>>q4>>q5;
+   cout<<"Message   " <<char(q1)<<char(q2)<<char(q3)<<char(q4)<<char(q5)<<endl;
 	
 	system("pause>0");
 }
