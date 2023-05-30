f1a3cf8daee163e4b7d120c8cf877be677a2177f
DIM <4ebotarov_dim@inbox.ru>
2023-05-30 21:21:26 +0300

    5 letters ASCII message

--- main.cpp
+++ main.cpp
@@ -10,7 +10,12 @@ printf("Hello ASCII \n");
   cout<< int ('a')	<<endl;
   cout<< int ('A')	<<endl;
   cout<<char (65)	<<endl;
-	
+  
+  char c1,c2,c3,c4,c5;
+  cout<<"Enter 5 letters : ";
+  cin>>c1>>c2>>c3>>c4>>c5 ;
+  cout<<"ASCII message " << int (c1) <<	" " << int (c2)
+   << " " << int (c3) << " " <<int(c4) << " " << int (c5) ;
 	
 	system("pause>0");
 }
